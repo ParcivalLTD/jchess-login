fb8ad59d75b865cc40353f559a5fccaecb817e4c
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-12-17 16:54:33 +0100

    Refactor database connection and user authentication

--- app.js
+++ app.js
@@ -1,119 +1,62 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const mysql = require("mysql");
-const cors = require("cors");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken"); // Import the jsonwebtoken library
-const { check, validationResult } = require("express-validator");
-const fs = require("fs");
-
-require("dotenv").config(); // Load environment variables from a .env file
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-// MySQL database connection
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectTimeout: 30000,
-  ssl: {
-    ca: fs.readFileSync("ca.pem"),
-    rejectUnauthorized: true,
-  },
+// Verbindung zu MongoDB
+mongoose.connect("mongodb+srv://juliangabriel570:@cluster0.76zba9m.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.use(bodyParser.json());
-app.use(cors({ origin: "https://web009.wifiooe.at" })); // Specify your frontend domain
-
-// Secret key for signing JWTs
-const jwtSecretKey = process.env.JWT_SECRET_KEY || "magomedstinky123"; // Replace with a secure secret key
-
-connection.getConnection((err, conn) => {
-  if (err) {
-    console.error("Error getting MySQL connection:", err);
-  } else {
-    console.log("Connected to MySQL database");
-    // Release the connection back to the pool
-    conn.release();
-  }
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
 });
 
-// Create the database and table if they don't exist
-connection.query(
-  `
-  CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME};
-  USE ${process.env.DB_NAME};
-  CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-  );
-`,
-  (err) => {
-    if (err) {
-      console.error("Error creating database and table:", err);
-    } else {
-      console.log("Database and table are ready");
-    }
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
   }
-);
+  next();
+});
 
-// Login endpoint
-app.post("/login", [check("username").notEmpty().escape(), check("password").notEmpty()], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+const User = mongoose.model("User", UserSchema);
 
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
-  // Hash the password before checking in the database
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-  const query = "SELECT * FROM users WHERE username = ? AND password = ?";
-  connection.query(query, [username, hashedPassword], (err, results) => {
-    if (err) {
-      console.error("Error executing MySQL query:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    if (results.length > 0) {
-      // Create and sign a JWT for successful login
-      const token = jwt.sign({ username }, jwtSecretKey, { expiresIn: "1h" });
-      res.json({ success: true, message: "Login successful", token });
-    } else {
-      res.status(401).json({ success: false, message: "Invalid username or password" });
-    }
-  });
-});
-
-// Registration endpoint
-app.post("/register", [check("username").notEmpty().escape(), check("password").notEmpty()], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+  if (!username || !password) {
+    return res.status(400).send("Missing username or password");
   }
 
+  const user = new User({ username, password });
+  await user.save();
+
+  res.status(201).send("User registered");
+});
+
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
-  // Hash the password before storing in the database
-  const hashedPassword = bcrypt.hashSync(password, 10);
+  const user = await User.findOne({ username });
 
-  const query = "INSERT INTO users (username, password) VALUES (?, ?)";
-  connection.query(query, [username, hashedPassword], (err, results) => {
-    if (err) {
-      console.error("Error executing MySQL query:", err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send("Invalid username or password");
+  }
 
-    res.json({ success: true, message: "Registration successful" });
-  });
+  const token = jwt.sign({ id: user._id }, "secret", { expiresIn: "1h" });
+
+  res.send({ token });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.post("/logout", (req, res) => {
+  // In einer echten Anwendung würden Sie hier das Token auf der Blacklist setzen
+  res.send("Logged out");
 });
+
+app.listen(3000, () => console.log("Server running on port 3000"));
