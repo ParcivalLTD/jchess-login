1277c4b478f8c502873a6c2547e2290e817492b3
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-12-17 17:58:34 +0100

    Update dependencies and remove unnecessary code

--- server.js
+++ server.js
@@ -1,97 +1,32 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const cookieSession = require("cookie-session");
 
 const app = express();
-app.use(cors);
-const PORT = process.env.PORT || 3000;
 
-const uri = "mongodb+srv://username:Yw013p9K6s5EEcvA@cluster.mongodb.net/myDB?retryWrites=true&w=majority&ssl=true";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "bezkoder-session",
+    keys: ["COOKIE_SECRET"], // should use as secret environment variable
+    httpOnly: true,
+  })
+);
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("MongoDB Connected...");
-  } catch (err) {
-    console.log(err);
-  } finally {
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
-
-const userSchema = new Schema({
-  username: String,
-  password: String,
-});
-
-const User = client.model("User", userSchema);
-app.use(bodyParser.json());
-
-// Register endpoint
-app.post("/register", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    // Check if the username already exists
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
-      return res.status(400).json({ message: "Username already exists" });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user
-    const newUser = new User({ username, password: hashedPassword });
-    await newUser.save();
-
-    res.status(201).json({ message: "User registered successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-// Login endpoint
-app.post("/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    // Check if the user exists
-    const user = await User.findOne({ username });
-    if (!user) {
-      return res.status(401).json({ message: "Invalid username or password" });
-    }
-
-    // Compare the password
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid username or password" });
-    }
-
-    // Create a JWT token
-    const token = jwt.sign({ username: user.username }, "dfgdgf", { expiresIn: "1h" });
-
-    res.json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}.`);
 });
