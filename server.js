79167944dc53bf56dc5c3bb3525461123d2d6b53
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-12-17 17:37:58 +0100

    Refactor server.js file

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Replace 'your_database_url' with your MongoDB connection string
-const mongoURI = 'mongodb+srv://juliangabriel570:#8zhp15G@cluster0.76zba9m.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = "mongodb+srv://juliangabriel570:#8zhp15G@cluster0.76zba9m.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const User = mongoose.model('User', {
+const User = mongoose.model("User", {
   username: String,
   password: String,
 });
@@ -19,14 +19,14 @@ const User = mongoose.model('User', {
 app.use(bodyParser.json());
 
 // Register endpoint
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
 
     // Check if the username already exists
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res.status(400).json({ message: 'Username already exists' });
+      return res.status(400).json({ message: "Username already exists" });
     }
 
     // Hash the password
@@ -36,33 +36,40 @@ app.post('/register', async (req, res) => {
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
 // Login endpoint
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
 
     // Check if the user exists
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: 'Invalid username or password' });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
 
     // Compare the password
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid username or password' });
+      return res.status(401).json({ message: "Invalid username or password" });
     }
 
     // Create a JWT token
-    const token = jwt.sign({ username: user.username }, 'your_secret_key', { expiresIn: '1h' });
+    const token = jwt.sign({ username: user.username }, "your_secret_key", { expiresIn: "1h" });
 
     res.json({ token });
   } catch (error) {
-  
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
